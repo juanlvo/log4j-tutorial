97fc50f9a30b52476c71917f68faa390e5af897d
Juan-Luis Vivas Occhipinti <juanlvo@gmail.com>
2019-01-21 17:05:51 +0100

    feat: Section 26 Lecture 146 Using configuration files

--- src/demo/LoggingConsole.java
+++ src/demo/LoggingConsole.java
@@ -4,15 +4,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /***
- * Section 26 Lecture 146
- * Using default configuration
+ * Section 26 Lecture 145
+ * Using configuration file
  * 
  * @author juanl
  *
  */
-public class LoggingDemo {
+public class LoggingConsole {
 	
-	private static final Logger log = LogManager.getLogger(LoggingDemo.class.getName());
+	private static final Logger log = LogManager.getLogger(LoggingConsole.class.getName());
 
 	public static void main(String[] args) {
 		log.debug("Debug Message Logged");
