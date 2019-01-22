520540fe2d81db57c78ff6b1ec90d9ba444c7d09
Juan-Luis Vivas Occhipinti <juanlvo@gmail.com>
2019-01-22 11:14:22 +0100

    feat: Section 26 Lecture 147 Using configuration file - File logging

---
+++ src/demo/LoggingFile.java
@@ -0,0 +1,23 @@
+package demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/***
+ * Section 26 Lecture 147
+ * Using configuration file - File logging
+ * 
+ * @author juanl
+ *
+ */
+public class LoggingFile {
+	
+	private static final Logger log = LogManager.getLogger(LoggingFile.class.getName());
+
+	public static void main(String[] args) {
+		log.debug("Debug Message Logged");
+		log.error("Error Message Logged");
+		log.fatal("Fatal Message Logged");
+	}
+
+}
