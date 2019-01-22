643a073a605a8c4b4c4476801ab6935e5bfe1c30
Juan-Luis Vivas Occhipinti <juanlvo@gmail.com>
2019-01-22 14:46:29 +0100

    feat: Section 26 Lecture 148 Working with custom loggers

--- src/letskodeit/LoggingFile.java
+++ src/letskodeit/LoggingFile.java
@@ -15,7 +15,9 @@ public class LoggingFile {
 	private static final Logger log = LogManager.getLogger(LoggingFile.class.getName());
 
 	public static void main(String[] args) {
+		log.trace("Trace Message Logged");
 		log.debug("Debug Message Logged");
+		log.info("Info Message Logged");
 		log.error("Error Message Logged");
 		log.fatal("Fatal Message Logged");
 	}
